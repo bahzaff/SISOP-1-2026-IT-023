7741baa66bd0ddfb35c436fca56c37d504ee5b2d
bahzaff <barrahzaf1@gmail.com>
2026-03-29 21:41:05 +0700

    revisi soal 2

--- soal_2/ekspedisi/peta-gunung-kawi/parserkoordinat.sh
+++ soal_2/ekspedisi/peta-gunung-kawi/parserkoordinat.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-
 grep -E '"id"|"site_name"|"latitude"|"longitude"' gsxtrack.json | \
 awk '
 /"id"/ { match($0, /"id": "([^"]+)"/, arr); id=arr[1] }
 /"site_name"/ { match($0, /"site_name": "([^"]+)"/, arr); site=arr[1] }
 /"latitude"/ { match($0, /"latitude": ([^,]+)/, arr); lat=arr[1] }
-/"longitude"/ { match($0, /"longitude": ([^,]+)/, arr); lon=arr[1]; print id", "site", "lat", "lon }
+/"longitude"/ { match($0, /"longitude": ([^,]+)/, arr); lon=arr[1]; print id "," site "," lat "," lon }
 ' | sort > titik-penting.txt
-
-cat titik-penting.txt
